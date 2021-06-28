b197b9f8a8d75d7658f420374875bac28e5dd642
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-28 14:34:01 +0100

    testeInjection

---
+++ src/main/java/com/example/demo/model/UserValidator.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+public class UserValidator {
+public boolean testId(int id)
+{
+return 	id>0;
+/*
+ * if(id>0)
+ * return true
+ * else return false;
+ */
+}
+}
--- src/test/java/com/example/demo/Tp2springboot1ApplicationTests.java
+++ src/test/java/com/example/demo/Tp2springboot1ApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.demo.model.UserValidator;
+
 @SpringBootTest
 class Tp2springboot1ApplicationTests {
-
+	UserValidator u;
+@BeforeEach
+void initialise()
+{
+	u=new UserValidator();
+	
+}
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void testMyid()
+	{
+	assertTrue(u.testId(13));
+		
+		
 	}
 
 }
